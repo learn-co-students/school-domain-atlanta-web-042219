0143c92f9246336480a637743d6fdca59c1b7b00
Will Jones <willjones2010@gmail.com>
2019-04-24 18:06:10 -0400

    need to finish sort

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+
+attr_accessor :roster, :name
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(name, num)
+    if !@roster[num]
+    @roster[num] = [name]
+  else
+    @roster[num] << name
+  end
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+  # @roster.sort
+  
+  end
+end
