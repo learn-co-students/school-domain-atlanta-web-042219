ab0c9d97931546c9cdc5317983e30edaea129a9c
github username <dekadekadeka@gmail.com>
2019-04-24 17:43:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+require 'pry'
+class School
+attr_reader :name, :roster, :student
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        #binding.pry
+        roster[grade] << student
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        roster.sort.to_h.map {|grade, array|
+        roster[grade] = array.sort
+    }
+    roster
+    end
+end
