5b9319be856f5a7df34738aa99c3f1e3582033a2
Paula Tulis <tulis.paula@gmail.com>
2019-04-25 17:45:22 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+      roster[grade] ||= []
+      #if roster[grade] is true or equal to anything, it stays what it is. If roster[grade] = false, set it equal to an empty array
+      roster[grade] << student_name #now you can shovel the students into the array
+    end
+
+def grade(level)
+  roster[level]
+end
+
+  def sort
+      new_hash = {}
+      roster.each do |grade, students|
+        new_hash[grade] = students.sort
+      end
+      new_hash
+    end
+  end
