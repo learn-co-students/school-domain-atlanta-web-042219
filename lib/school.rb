f8bcc0525725779235b22f68ac5b6d613978e857
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-25 17:51:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+# code here!require 'pry'
+class School
+  attr_reader :name, :roster, :grade
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+
+    @roster.collect do |key, value|
+      @roster[key] = value.sort
+    end
+    @roster.sort_by {|key, value| key}.to_h
+
+
+  end
+end
