c67d85e520e6e42f466e25c968f80dddbccbc33e
github username <github email address>
2019-05-24 15:00:01 -0400

    refreshing during a ga summer HEAT wave

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,30 @@
 class School
     attr_accessor :name, :roster
+
     def initialize(name)
         @name = name
         @roster = {}
     end 
-    
-    def add_student(student, grade)
-        roster[grade] ||= []
-        roster[grade] << student
+
+    def add_student(stu_name, grade) 
+        if @roster[grade] == nil
+            @roster[grade] = []
+            @roster[grade] << stu_name
+        else 
+            @roster[grade] << stu_name
+        end  
     end 
 
     def grade(grade)
-        roster.find do |key, value|
-            if key == grade
-                return value
-            end 
-        end 
+       roster[grade]
     end 
 
     def sort
-        new_hash = {}
-        roster.each do |key, value|
-            roster[key] = value.sort
+        sorted = {}
+        roster.each do | grade, stu_name|
+            sorted[grade] = stu_name.sort
         end 
-
-        return roster
+         sorted
     end 
-
-
 
 end 
